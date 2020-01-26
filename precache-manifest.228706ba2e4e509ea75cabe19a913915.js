51b06b6379748abbe57543d126c2e28cfb6dbdfe
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2020-01-25 21:12:52 -0500

    Updates

---
+++ precache-manifest.228706ba2e4e509ea75cabe19a913915.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f82b55bc99fcf91e998ed887487ea206",
+    "url": "/gitdeploy/index.html"
+  },
+  {
+    "revision": "a95e3fedae52dfaf6e52",
+    "url": "/gitdeploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bf5fed764a5359d7a48c",
+    "url": "/gitdeploy/static/js/2.4856285b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gitdeploy/static/js/2.4856285b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a95e3fedae52dfaf6e52",
+    "url": "/gitdeploy/static/js/main.7661a372.chunk.js"
+  },
+  {
+    "revision": "dbe9a9b73fce360ae114",
+    "url": "/gitdeploy/static/js/runtime-main.fa8db239.js"
+  }
+]);
